986c092695c91005275e83b4f9388df2e9ada2fd
Ryan Tan <ryantan.mail@gmail.com>
2023-04-18 23:32:03 -0600

    proof of concept working

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ require('dotenv').config()
 //Creation of express object
 const app = express();
 
+//TODO: Not scalable at the moment. Will have to use a database to store for each user
+let accessToken = "";
+let refreshToken = "";
+
 //Login and authenticate in Spotify
 app.get('/login', (req, res)=> {
 	const scope = "user-top-read";
@@ -18,10 +22,46 @@ app.get('/login', (req, res)=> {
 	res.redirect(authUrl + authSearchParam.toString());
 });
 
-app.get('/callback', (req, res) => {
-	res.send("hello world");
+//Callback after Spotify authentication
+app.get('/callback', async (req, res) => {
+	const code = req.query.code || null;
+	const state = req.query.code || null;
+	if (state == null){
+		res.redirect('/error');
+	}
+    let tokenHeaders = new Headers();
+	tokenHeaders.append("Authorization", "Basic " + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64")));
+	let tokenUrl = "https://accounts.spotify.com/api/token"
+	let tokenBody = new URLSearchParams();
+	tokenBody.append("code", code);
+	tokenBody.append("redirect_uri", process.env.SPOTIFY_REDIRECT_URI);
+	tokenBody.append("grant_type", "authorization_code");
+	const authResponse = await fetch(tokenUrl, {method: "POST", headers: tokenHeaders, body: tokenBody});
+	const authData = await authResponse.json();
+	accessToken = authData.access_token;
+	refreshToken = authData.refresh_token;
+	res.redirect("/topfive");
 });
 
+//Testing getting top five albums
+app.get("/topfive", async(req, res)=>{
+	let trackUrl = "https://api.spotify.com/v1/me/top/tracks?";
+	let topFiveHeaders = new Headers();
+	topFiveHeaders.append("Authorization", "Bearer " + accessToken);
+	let topFiveParams = new URLSearchParams();
+	topFiveParams.append('limit', 5);
+
+	let response = await fetch(trackUrl + topFiveParams, {method: "GET", headers: topFiveHeaders});
+	let data = await response.json();
+	res.send(data);
+});
+
+//Error
+app.get('/error', (req, res)=> {
+	res.send("An error has occured :(");
+});
+
+// localhost:3000
 const port = 3000;
 app.listen(port, ()=>{
 	console.log(`Server is listening on port ${port}`);
